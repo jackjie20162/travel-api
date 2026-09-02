40d33404b19dbd77446f1a7a8912f084a43ee923
jackjie20162 <610841147@qq.com>
2026-09-02 19:15:55 +0800

    feat(payment): expose payment RPC client

--- internal/svc/servicecontext.go
+++ internal/svc/servicecontext.go
@@ -1,23 +1,25 @@
 package svc
 
 import (
-	"gitee.com/meinongyihe/travel-api/internal/config"
-	"gitee.com/meinongyihe/travel-rpc/travel"
-	"google.golang.org/grpc"
+    "gitee.com/meinongyihe/travel-api/internal/config"
+    "gitee.com/meinongyihe/travel-rpc/travel"
+    "google.golang.org/grpc"
 )
 
 type ServiceContext struct {
-	Config          config.Config
-	CatalogClient   travel.CatalogServiceClient
-	InventoryClient travel.InventoryServiceClient
-	OrderClient     travel.OrderServiceClient
+    Config          config.Config
+    CatalogClient   travel.CatalogServiceClient
+    InventoryClient travel.InventoryServiceClient
+    OrderClient     travel.OrderServiceClient
+    PaymentClient   travel.PaymentServiceClient
 }
 
 func NewServiceContext(c config.Config, conn grpc.ClientConnInterface) *ServiceContext {
-	return &ServiceContext{
-		Config:          c,
-		CatalogClient:   travel.NewCatalogServiceClient(conn),
-		InventoryClient: travel.NewInventoryServiceClient(conn),
-		OrderClient:     travel.NewOrderServiceClient(conn),
-	}
+    return &ServiceContext{
+        Config: c,
+        CatalogClient: travel.NewCatalogServiceClient(conn),
+        InventoryClient: travel.NewInventoryServiceClient(conn),
+        OrderClient: travel.NewOrderServiceClient(conn),
+        PaymentClient: travel.NewPaymentServiceClient(conn),
+    }
 }
