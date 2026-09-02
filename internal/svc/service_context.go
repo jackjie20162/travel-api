5b35e01adefbc10bc532188088a3f291a408eacd
jackjie20162 <610841147@qq.com>
2026-09-02 19:31:52 +0800

    feat(travel-api): wire travel RPC payment client

--- internal/svc/service_context.go
+++ internal/svc/service_context.go
@@ -1,11 +1,25 @@
 package svc
 
-import "gitee.com/meinongyihe/travel-api/internal/config"
+import (
+    "gitee.com/meinongyihe/travel-api/internal/config"
+    "gitee.com/meinongyihe/travel-rpc/travel"
+    "google.golang.org/grpc"
+)
 
 type ServiceContext struct {
-	Config config.Config
+    Config          config.Config
+    CatalogClient   travel.CatalogServiceClient
+    InventoryClient travel.InventoryServiceClient
+    OrderClient     travel.OrderServiceClient
+    PaymentClient   travel.PaymentServiceClient
 }
 
-func NewServiceContext(c config.Config) *ServiceContext {
-	return &ServiceContext{Config: c}
+func NewServiceContext(c config.Config, conn grpc.ClientConnInterface) *ServiceContext {
+    return &ServiceContext{
+        Config:          c,
+        CatalogClient:   travel.NewCatalogServiceClient(conn),
+        InventoryClient: travel.NewInventoryServiceClient(conn),
+        OrderClient:     travel.NewOrderServiceClient(conn),
+        PaymentClient:   travel.NewPaymentServiceClient(conn),
+    }
 }
