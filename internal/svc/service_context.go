43f1e811a22eaef9735b3da2feb4c15e6e6a2b55
jackjie20162 <610841147@qq.com>
2026-09-02 19:56:12 +0800

    feat(travel-api): wire PayPal provider into service context

--- internal/svc/service_context.go
+++ internal/svc/service_context.go
@@ -2,6 +2,7 @@ package svc
 
 import (
     "gitee.com/meinongyihe/travel-api/internal/config"
+    "gitee.com/meinongyihe/travel-api/internal/paypal"
     "gitee.com/meinongyihe/travel-rpc/travel"
     "google.golang.org/grpc"
 )
@@ -12,14 +13,16 @@ type ServiceContext struct {
     InventoryClient travel.InventoryServiceClient
     OrderClient     travel.OrderServiceClient
     PaymentClient   travel.PaymentServiceClient
+    PayPal          *paypal.Provider
 }
 
 func NewServiceContext(c config.Config, conn grpc.ClientConnInterface) *ServiceContext {
     return &ServiceContext{
-        Config:          c,
-        CatalogClient:   travel.NewCatalogServiceClient(conn),
+        Config: c,
+        CatalogClient: travel.NewCatalogServiceClient(conn),
         InventoryClient: travel.NewInventoryServiceClient(conn),
-        OrderClient:     travel.NewOrderServiceClient(conn),
-        PaymentClient:   travel.NewPaymentServiceClient(conn),
+        OrderClient: travel.NewOrderServiceClient(conn),
+        PaymentClient: travel.NewPaymentServiceClient(conn),
+        PayPal: paypal.New(paypal.Config{Enabled:c.Payment.PayPal.Enabled,BaseURL:c.Payment.PayPal.BaseURL,ClientID:c.Payment.PayPal.ClientID,Secret:c.Payment.PayPal.Secret,WebhookID:c.Payment.PayPal.WebhookID,ReturnURL:c.Payment.PayPal.ReturnURL,CancelURL:c.Payment.PayPal.CancelURL}),
     }
 }
