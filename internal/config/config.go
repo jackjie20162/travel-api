9921c47d1f6cf68c3c994be5d2f9d36bc4ba7a91
jackjie20162 <610841147@qq.com>
2026-09-02 19:15:47 +0800

    feat(payment): add PayPal configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,8 +3,16 @@ package config
 import "github.com/zeromicro/go-zero/rest"
 
 type Config struct {
-	rest.RestConf
-	TravelRpc struct {
-		Target string
-	}
+    rest.RestConf
+    TravelRpc struct { Target string }
+    Payment struct {
+        PayPal struct {
+            Enabled   bool
+            BaseURL   string
+            ClientID  string
+            Secret    string
+            ReturnURL string
+            CancelURL string
+        }
+    }
 }
