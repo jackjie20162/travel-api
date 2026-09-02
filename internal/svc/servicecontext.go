25d6ef84c74c1806e844dc561111fd18ef7217a4
jackjie20162 <610841147@qq.com>
2026-09-02 18:18:13 +0800

    feat(travel-api): add RPC-backed service context

---
+++ internal/svc/servicecontext.go
@@ -0,0 +1,23 @@
+package svc
+
+import (
+	"gitee.com/meinongyihe/travel-api/internal/config"
+	"gitee.com/meinongyihe/travel-rpc/travel"
+	"google.golang.org/grpc"
+)
+
+type ServiceContext struct {
+	Config          config.Config
+	CatalogClient   travel.CatalogServiceClient
+	InventoryClient travel.InventoryServiceClient
+	OrderClient     travel.OrderServiceClient
+}
+
+func NewServiceContext(c config.Config, conn grpc.ClientConnInterface) *ServiceContext {
+	return &ServiceContext{
+		Config:          c,
+		CatalogClient:   travel.NewCatalogServiceClient(conn),
+		InventoryClient: travel.NewInventoryServiceClient(conn),
+		OrderClient:     travel.NewOrderServiceClient(conn),
+	}
+}
